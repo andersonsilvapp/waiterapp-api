0077aa20ac5929149a3373a3a37429eeb55b2a8a
Anderson <andersonsilvapp@gmail.com>
2023-05-24 18:34:38 -0300

    feat: dotenv config

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import http from 'node:http';
 
 import express from 'express';
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
 import { Server } from 'socket.io';
 
 import { router } from './router';
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
