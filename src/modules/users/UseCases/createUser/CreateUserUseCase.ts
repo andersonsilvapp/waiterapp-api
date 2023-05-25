a992de9f93e061e60bb949493e99e45e3555e65e
Anderson <andersonsilvapp@gmail.com>
2023-05-25 18:05:37 -0300

    refactor: createUser divided into controller, useCase and repository

---
+++ src/modules/users/UseCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt';
+
+import { usersRepository } from '../../infra/mongoose/repositories/UsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserUseCase {
+  async execute({ name, email, password }: IRequest) {
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const user = await usersRepository.create({ name, email, password: passwordHash });
+
+    return user;
+  }
+}
+
+const createUserUseCase = new CreateUserUseCase();
+
+export { createUserUseCase };
---
+++ src/modules/users/infra/mongoose/repositories/UsersRepository.ts
@@ -0,0 +1,28 @@
+import { ICreateUserDTO } from '../../../dtos/ICreateUserDTO';
+
+import { User } from '../models/User';
+
+class UsersRepository {
+  async create({ name, email, password }: ICreateUserDTO) {
+
+    const user = await User.create({ name, email, password });
+
+    return user;
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await User.findOne().where('email').equals(email);
+
+    return user;
+  }
+
+  async getUserById(id: string) {
+    const user = await User.findById(id, '-password');
+
+    return user;
+  }
+}
+
+const usersRepository = new UsersRepository();
+
+export { usersRepository };
