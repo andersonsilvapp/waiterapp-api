99d4a02d06dad3a2372f1fa37a086130947e6c6d
Anderson <andersonsilvapp@gmail.com>
2023-05-25 16:34:24 -0300

    refactor: product routes adjusted

---
+++ src/shared/infra/http/routes/products.routes.ts
@@ -0,0 +1,36 @@
+import path from 'node:path';
+import { Router } from 'express';
+import { createProductController } from '../../../../modules/products/UseCases/createProduct/CreateProductController';
+import multer from 'multer';
+import { deleteProductController } from '../../../../modules/products/UseCases/deleteProduct/DeleteProductController';
+import { editProductController } from '../../../../modules/products/UseCases/editProduct/EditProductController';
+import { editProductPriceController } from '../../../../modules/products/UseCases/editProductPrice/EditProductPriceController';
+import { editProductStatusController } from '../../../../modules/products/UseCases/editProductStatus/EditProductStatusController';
+import { listProductController } from '../../../../modules/products/UseCases/listProduct/ListProductController';
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '../../../../../', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    }
+  }),
+});
+
+const productsRoutes = Router();
+
+productsRoutes.post('/', upload.single('image'),  createProductController.handle);
+
+productsRoutes.get('/', listProductController.handle);
+
+productsRoutes.put('/:productId', upload.single('image'), editProductController.handle);
+
+productsRoutes.patch('/:productId/price', editProductPriceController.handle);
+
+productsRoutes.patch('/:productId/status', editProductStatusController.handle);
+
+productsRoutes.delete('/:productId', deleteProductController.handle);
+
+export { productsRoutes };
